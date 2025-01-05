d5f9955d8400eaa4d9ed038c4197bdf5be604c35
tusharbhatiav3 <tbhatia@kubikk.io>
2025-01-06 02:37:07 +0530

    Fix server configuration for Heroku deployment

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ connectDB();
 
 // Middleware
 app.use(cors({
-  origin: ["*"], 
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true,
-  }));
+  origin: ['http://localhost:3001', 'https://friend-finder-app-ui.vercel.app'],  
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}));
 
 app.use(express.json());
 
